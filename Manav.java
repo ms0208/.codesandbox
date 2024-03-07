7329c34df77db3861ea646069a44c9e2939c3a11
ms0208 <manav.8230680@gmail.com>
2024-03-07 11:26:26 +0530

    first commit

---
+++ Manav.java
@@ -0,0 +1,7 @@
+class Manav
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Manav");
+    }
+}
---
+++ manav.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Manav";
+    return 0;
+}
